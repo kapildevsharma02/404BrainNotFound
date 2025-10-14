c64908a7bf3984f60341b1e842a3556d935b1181
2270366 <2270366@LTIN511242.cts.com>
2025-10-14 13:03:01 +0530

    Adding the method findFirstRepeatingDigit...

--- src/com/practice/string/BitSet.java
+++ src/com/practice/string/BitSet.java
@@ -1,6 +1,26 @@
 package com.practice.string;
 
+import java.util.HashMap;
+import java.util.Map;
 public class BitSet {
+
+	public static int findFirstRepeatingDigit(String digitPattern) {
+		// Write your code here.
+		Map<Character, Integer> map = new HashMap<>();
+		for (char c : digitPattern.toCharArray()) {
+			if (map.containsKey(c)) {
+				map.put(c, map.get(c) + 1);
+			} else {
+				map.put(c, 1);
+			}
+
+			if (map.get(c) > 1) {
+				int number = c - 48;
+				return number;
+			}
+		}
+		return -1;
+	}
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
