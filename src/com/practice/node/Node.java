64673b59491cea651ddc99899e5773a5e6bc9fb4
Kapil <2270366@LTIN511242.cts.com>
2025-11-17 11:11:31 +0530

    Adding the class Node...

---
+++ src/com/practice/node/Node.java
@@ -0,0 +1,25 @@
+package com.practice.node;
+
+public class Node {
+	public int data;
+	public Node next;
+	public Node prev;
+
+	public Node() {
+		this.data = 0;
+		this.next = null;
+		this.prev = null;
+	}
+
+	public Node(int data) {
+		this.data = data;
+		this.next = null;
+		this.prev = null;
+	}
+
+	public Node(int data, Node next) {
+		this.data = data;
+		this.next = next;
+		this.prev = next;
+	}
+};
