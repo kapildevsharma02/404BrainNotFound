6d53847f892f59f79cd2f66af911406c40763005
Kapil Dev Sharma <71621562+kapildevsharma02@users.noreply.github.com>
2025-12-20 10:16:47 +0530

    adding another approach of the left rotate an array by one...

--- src/com/practice/array/LeftRotateAnArrayByOne.java
+++ src/com/practice/array/LeftRotateAnArrayByOne.java
@@ -3,12 +3,20 @@ package com.practice.array;
 public class LeftRotateAnArrayByOne {
 
 	static int[] rotateArray(int[] arr, int n) {
-		// Write your code here.
+		// // Write your code here.
+		// for (int i = 0; i < n - 1; i++) {
+		// 	int temp = arr[i];
+		// 	arr[i] = arr[i + 1];
+		// 	arr[i + 1] = temp;
+		// }
+		// return arr;
+		
+		//Another Apporach of the left rotate an array by one
+		int temp = arr[0];
 		for (int i = 0; i < n - 1; i++) {
-			int temp = arr[i];
 			arr[i] = arr[i + 1];
-			arr[i + 1] = temp;
 		}
+		arr[n - 1] = temp;
 		return arr;
 	}
 
@@ -21,3 +29,4 @@ public class LeftRotateAnArrayByOne {
 	}
 
 }
+
