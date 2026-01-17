749d59fb4aed62db78b791c07fd490da8dc89e45
Kapil <2270366@LTIN511242.cts.com>
2026-01-17 09:06:03 +0530

    Easy question asked in company like google etc..

---
+++ src/com/practice/string/toLowerCase.java
@@ -0,0 +1,15 @@
+package com.practice.string;
+
+public class toLowerCase {
+	public static String toLowerCase(String str) {
+		// Write your code here..
+		return str.toLowerCase();
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String ans = toLowerCase("ABCHedg");
+		System.out.println(ans);
+	}
+
+}
