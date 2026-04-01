5ff04d1e5d696f66096dcda31cd764a3a5bf9a53
Kapil <2270366@LTIN511242.cts.com>
2026-04-01 20:07:13 +0530

    one of the question asked in companies like optum, oracle, ibm etc...

---
+++ src/com/practice/normalvariableloopquestions/ArmstrongNumber.java
@@ -0,0 +1,39 @@
+package com.practice.normalvariableloopquestions;
+
+public class ArmstrongNumber {
+
+	public static boolean isArmstrong(int num) {
+		// write your code here;
+		int digit = digits(num);
+		int temp = num;
+		int sum = 0;
+		while (temp != 0) {
+			sum += Math.pow(temp % 10, digit);
+			temp = temp / 10;
+		}
+		if (sum == num) {
+			return true;
+		}
+		return false;
+	}
+
+	public static int digits(int num) {
+		int temp = num;
+		int count = 0;
+		while (temp != 0) {
+			temp = temp / 10;
+			count++;
+		}
+		return count;
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		boolean ans = isArmstrong(153);
+		if (ans == true) {
+			System.out.println("Yes its armstrong number");
+		} else {
+			System.out.println("No its not armstrong number");
+		}
+	}
+}
